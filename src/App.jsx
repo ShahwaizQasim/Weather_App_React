2e02b6427fb995a36432c99bcbd38ebece080610
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-12-04 00:36:03 +0500

    axios library install

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import './App.css'
 function App() {
   const [weather, setWeather] = useState(0);
   console.log('weather', weather);
+
+const fetchApi = () => {
+    
+}
+
+  const handleClick = () => {
+    fetchApi();
+  }
+
   return (
     <div>
       <h2>Weather App</h2>
@@ -13,7 +22,7 @@ function App() {
           className='py-1 px-2 border-2 rounded-md mt-4'
           value={weather} onChange={(e) => setWeather(e.target.value)} />
       </form>
-      <button className='py-1 mt-4 text-[12px] px-2 border-2 rounded-md'>Check Weather</button>
+      <button className='py-1 mt-4 text-[12px] px-2 border-2 rounded-md' onClick={handleClick}>Get Weather</button>
     </div>
   )
 }
