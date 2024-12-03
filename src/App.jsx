9f85b6042828fbd65b25f181971685787d214179
ShahwaizQasim <syedshahwaiz32@gmail.com>
2024-12-03 10:38:42 +0500

    basic code structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-return(
-  <div>
-    <h2>Hello World!</h2>
-  </div>
+  const [weather, setWeather] = useState(0);
+  console.log('weather', weather);
+  return (
+    <div>
+      <h2>Weather App</h2>
+      <form>
+        <input type="text" placeholder='Enter Weather'
+          className='py-1 px-2 border-2 rounded-md mt-4'
+          value={weather} onChange={(e) => setWeather(e.target.value)} />
+      </form>
+      <button className='py-1 mt-4 text-[12px] px-2 border-2 rounded-md'>Check Weather</button>
+    </div>
   )
 }
 
